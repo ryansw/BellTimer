64fb898b9f25629d13aa9445b15eacb41a14df39
Ryan White <ryanswhite98@gmail.com>
2019-03-15 16:30:18 -0700

    Moved to a router setup

--- interface/src/index.js
+++ interface/src/index.js
@@ -1,12 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Landing from "./page/Landing";
+import NotFound from "./page/NotFound";
+import Player from "./page/Player";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+  <Router>
+    <Switch>
+      <Route exact path={"/"} component={Landing} />
+      <Route path={"/play/:system"} component={Player} />
+      <Route component={NotFound} />
+    </Switch>
+  </Router>,
+  document.getElementById("root")
+);
