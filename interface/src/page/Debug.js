eb75d1d8ac03f1b862d7fe28ad3a650a1b0d2864
Ryan White <ryanswhite98@gmail.com>
2019-03-18 18:54:26 -0700

    Built code to convert old format to new format

--- interface/src/page/Debug.js
+++ interface/src/page/Debug.js
@@ -16,6 +16,68 @@ class Player extends Component {
       curtimet: "Current Time",
       curtime: "18:00:00"
     };
+    let data = this.data;
+    console.log(this.data);
+    this.newdata = {};
+    this.newdata["Systems"] = {};
+    this.newdata["Systems"][this.data.Path] = {
+      Details: {
+        Name: this.data.Name,
+        Title: this.data.Name,
+        Description: this.data.Description
+      },
+      Schedules: {},
+      Events: {},
+      Weekdays: {
+        Sunday: false,
+        Monday: false,
+        Tuesday: false,
+        Wednesday: false,
+        Thursday: false,
+        Friday: false,
+        Saturday: false
+      },
+      Specials: {},
+      Owner: 0,
+      Editors: {}
+    };
+    for (let x of data.Schedules) {
+      let data = { Title: x.DisplayName, Description: x.Description };
+      this.newdata.Systems[this.data.Path].Schedules[x.Name] = data;
+      data = {};
+      for (let y of x.Events) {
+        data[y.Name] = {
+          Time: y.Time,
+          Description: y.Description,
+          Title: y.Name + " in",
+          Bottom: y.Name + " at",
+          Notice: y.Message
+        };
+      }
+      this.newdata.Systems[this.data.Path].Events[x.Name] = data;
+      if (x.Reason === 1) {
+        if (x.WDays & (1 << 0))
+          this.newdata.Systems[this.data.Path].Weekdays.Sunday = x.Name;
+        if (x.WDays & (1 << 1))
+          this.newdata.Systems[this.data.Path].Weekdays.Monday = x.Name;
+        if (x.WDays & (1 << 2))
+          this.newdata.Systems[this.data.Path].Weekdays.Tuesday = x.Name;
+        if (x.WDays & (1 << 3))
+          this.newdata.Systems[this.data.Path].Weekdays.Wednesday = x.Name;
+        if (x.WDays & (1 << 4))
+          this.newdata.Systems[this.data.Path].Weekdays.Thursday = x.Name;
+        if (x.WDays & (1 << 5))
+          this.newdata.Systems[this.data.Path].Weekdays.Friday = x.Name;
+        if (x.WDays & (1 << 6))
+          this.newdata.Systems[this.data.Path].Weekdays.Saturday = x.Name;
+      }
+      if (x.Reason === 2) {
+        for (let y of x.Dates) {
+          this.newdata.Systems[this.data.Path].Specials[y] = x.Name;
+        }
+      }
+    }
+    console.log(this.newdata);
   }
 
   update = () => {
@@ -47,6 +109,12 @@ class Player extends Component {
         <div style={{ wordWrap: "break-word" }}>
           {" "}
           {JSON.stringify(this.dispdata)}{" "}
+        </div>
+        this.newdata:
+        <br />
+        <div style={{ wordWrap: "break-word" }}>
+          {" "}
+          {JSON.stringify(this.newdata)}{" "}
         </div>
       </div>
     );
