1886401f984873c617cad83195a9a8be4a116d8d
Ryan White <ryanswhite98@gmail.com>
2019-03-19 22:40:06 -0700

    clearly important update to 404

--- interface/src/page/NotFound.js
+++ interface/src/page/NotFound.js
@@ -1,10 +1,13 @@
 import React, { Component } from "react";
+import pinkie from "../graphics/pinkie-confused.png";
 
 class NotFound extends Component {
   render() {
     return (
       <div className="NotFound">
-        The link you have used appears to be broken.
+        How'd you get here?
+        <br />
+        <img src={pinkie} alt="Pinkie Pie Confused" height="200" />
       </div>
     );
   }
