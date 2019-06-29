3ac15622835e1193dd1d98f082d1b58003a1bb27
Ryan White <Ryan@RyanSW.com>
2019-06-29 16:40:34 -0700

    New calendar format

--- interface/src/component/Calendar/calendar.js
+++ interface/src/component/Calendar/calendar.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import { withFirebase } from "../Firebase";
 import Datemark from "./datemark";
 
+import "../../styles/component/Calendar.css";
+
 const monthName = [
   "January",
   "February",
@@ -29,6 +31,8 @@ class Calendar extends Component {
     };
 
     this.dateclicked = this.dateclicked.bind(this);
+    this.BtnNextMo = this.BtnNextMo.bind(this);
+    this.BtnPrevMo = this.BtnPrevMo.bind(this);
   }
 
   dateclicked = (number, month) => {
@@ -40,101 +44,94 @@ class Calendar extends Component {
     this.props.onClick(year + "-" + (month + 1) + "-" + number);
   };
 
+  BtnNextMo = () => {
+    let nm = this.state.month + 1;
+    let ny = this.state.year;
+    if (nm < 0) {
+      ny--;
+      nm = 11;
+    }
+    if (nm > 11) {
+      ny++;
+      nm = 0;
+    }
+    this.setState({
+      month: nm,
+      year: ny
+    });
+  };
+
+  BtnPrevMo = () => {
+    let nm = this.state.month - 1;
+    let ny = this.state.year;
+    if (nm < 0) {
+      ny--;
+      nm = 11;
+    }
+    if (nm > 11) {
+      ny++;
+      nm = 0;
+    }
+    this.setState({
+      month: nm,
+      year: ny
+    });
+  };
+
   render() {
     let monthlength = new Date(
         this.state.year,
         this.state.month + 1,
         0
       ).getDate(),
-      startday = new Date(this.state.year, this.state.month, 1).getDay(),
-      rows = Math.ceil((monthlength + startday) / 7),
-      pmonthlength = new Date(this.state.year, this.state.month, 0).getDate();
-    let dates = [];
-    for (let row = 0; row < rows; row++) {
-      let roww = [];
-      for (let col = 0; col < 7; col++) {
-        let c = {
-          month: this.state.month,
-          current: true,
-          number: col + row * 7 - startday + 1
-        };
-        if (c.number > monthlength)
-          c = {
-            month: (this.state.month + 1) % 12,
-            current: false,
-            number: c.number % monthlength
-          };
-        if (c.number < 1)
-          c = {
-            month: (this.state.month + 11) % 12,
-            current: false,
-            number: c.number + pmonthlength
-          };
-        roww.push(c);
-      }
-      dates.push(roww);
-    }
+      startday = new Date(this.state.year, this.state.month, 1).getDay();
 
     return (
       <div className="Calendar">
-        <span
-          onClick={() => {
-            let nm = this.state.month - 1;
-            let ny = this.state.year;
-            if (nm < 0) {
-              ny--;
-              nm = 11;
-            }
-            if (nm > 11) {
-              ny++;
-              nm = 0;
-            }
-            this.setState({
-              month: nm,
-              year: ny
-            });
-          }}
-        >
-          Back
-        </span>
-        {monthName[this.state.month]} {this.state.year}
-        <span
-          onClick={() => {
-            let nm = this.state.month + 1;
-            let ny = this.state.year;
-            if (nm < 0) {
-              ny--;
-              nm = 11;
-            }
-            if (nm > 11) {
-              ny++;
-              nm = 0;
-            }
-            this.setState({
-              month: nm,
-              year: ny
-            });
-          }}
-        >
-          Forward
-        </span>
-        <table>
-          <tbody>
-            {dates.map((comp, i) => {
-              return (
-                <tr key={"Calrow" + i}>
-                  {comp.map((comp, j) => {
-                    return (
-                      <td key={"Calcol" + i + "," + j}>
-                        <Datemark {...comp} onClick={this.dateclicked} />
-                      </td>
-                    );
-                  })}
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
+        <div className="CalHeader">
+          <div className="CalPrev" onClick={this.BtnPrevMo}>
+            &lt;==
+          </div>
+          <div className="CalTitle">
+            {monthName[this.state.month]} <b>{this.state.year}</b>
+          </div>
+          <div className="CalNext" onClick={this.BtnNextMo}>
+            ==&gt;
+          </div>
+        </div>
+        <div className="CalBody">
+          {[...Array(startday).keys()].map((comp, i) => {
+            return (
+              <Datemark
+                LastMo="true"
+                Date={
+                  new Date(this.state.year, this.state.month, i + 1 - startday)
+                }
+              />
+            );
+          })}
+          {[...Array(monthlength).keys()].map((comp, i) => {
+            return (
+              <Datemark
+                Date={new Date(this.state.year, this.state.month, i + 1)}
+              />
+            );
+          })}
+          {[...Array(42 - monthlength - startday).keys()].map((comp, i) => {
+            return (
+              <Datemark
+                NextMo="true"
+                Date={
+                  new Date(
+                    this.state.year,
+                    this.state.month,
+                    monthlength + i + 1
+                  )
+                }
+              />
+            );
+          })}
+        </div>
       </div>
     );
   }
